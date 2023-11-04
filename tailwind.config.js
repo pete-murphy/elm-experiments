6735718dec47b09c470b92f9b6a7bf592e49df87
Peter Murphy <26548438+ptrfrncsmrph@users.noreply.github.com>
2023-11-04 11:09:12 -0400

    feat: Add "Work Sans" font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,35 @@
 module.exports = {
-    content: [
-        './index.html',
-        './src/js/main.js',
-        './src/css/main.css',
-        './src/elm/**/*.elm'
-    ],
-    theme: {
-        extend: {},
-        fontFamily: {
-            sans: ["Name Sans", "sans-serif"]
-        }
+  content: [
+    "./index.html",
+    "./src/js/main.js",
+    "./src/css/main.css",
+    "./src/elm/**/*.elm",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: [
+        "Work Sans",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
